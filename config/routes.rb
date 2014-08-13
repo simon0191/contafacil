587a0f389dccee114c0af87d95790132b4251ef5
simon0191 <simon0191@gmail.com>
2014-08-12 20:01:22 -0500

    Add company scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :companies
+
   resources :account_records
 
   resources :monthly_balances
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806223613) do
+ActiveRecord::Schema.define(version: 20140813005114) do
 
   create_table "account_records", force: true do |t|
     t.integer  "account_id"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 20140806223613) do
   create_table "accounts", force: true do |t|
     t.integer  "puc_id"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "company_id"
+  end
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.string   "nit"
+    t.text     "address"
+    t.string   "phone_number"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
